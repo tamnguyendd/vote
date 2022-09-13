d56b9d52a4f9e5fa37b55269f91c82148c763602
tamncrikkei <tamnc@rikkeisoft.com>
2022-09-13 22:44:49 +0900

    client

---
+++ client/components/constant.js
@@ -0,0 +1,6 @@
+export const constant_value = {
+    API_URL: {
+        get_item: "http://localhost:5000/item/get_items",
+        update_voted: "http://localhost:5000/item/update_voted"
+    }
+};
